0756eaca6e4b54996254f7c2c23430aa9bf3351d
Jack Kolb <Jack.Kolb@Workiva.com>
2019-04-30 15:16:14 -0600

    initial try

--- src/HaarWavelet2D.java
+++ src/HaarWavelet2D.java
@@ -1,11 +1,11 @@
 
 public class HaarWavelet2D {
 	double[][] matrix;
-	
+
 	public HaarWavelet2D(double[][] data){
 		matrix = data;
 	}
-	
+
 	public void show(){
 		for (int i = 0 ; i < matrix.length ; i++){
 			for (int j = 0 ; j < matrix[i].length ; j++){
@@ -14,7 +14,7 @@ public class HaarWavelet2D {
 			System.out.println("");
 		}
 	}
-	
+
 	public void transpose(){
 		double[][] transposed = new double[matrix[0].length][matrix.length];
 		for (int i = 0 ; i < matrix.length ; i++){
@@ -22,24 +22,24 @@ public class HaarWavelet2D {
 				transposed[j][i] = matrix[i][j];
 			}
 		}
-		
+
 		matrix = transposed;
 	}
-	
+
 	public void standardDecomposition(){
 		processRows();
 		transpose();
 		processRows();
 		transpose();
 	}
-	
+
 	public void processRows(){
 		int size = matrix.length;
 		for (int i = 0 ; i < size ; i++){
 			matrix[i] = decompositionRow(matrix[i]);
 		}
 	}
-	
+
 	public double[] decompositionRow(double[] row){
 		int steps = row.length;
 		while (steps > 1){
@@ -48,19 +48,20 @@ public class HaarWavelet2D {
 		}
 		return row;
 	}
-	
-	public double[] decompositionStep(int step, double[] row){
+
+	public double[] decompositionStep(int steps, double[] row){
 		double[] newRow = new double[row.length];
 		System.arraycopy( row, 0, newRow, 0, row.length );
-		
-		for (int i=0 ; i < step/2 ; i++){
+
+		for (int i=0 ; i < steps/2 ; i++){
+
 			newRow[i] = (row[2*i + 1] + row[2*i])/ 2;
-			newRow[step/2 + i] = (row[(2*i)] - row[(2*i + 1)])/2;	
+			newRow[steps/2 + i] = (row[(2*i)] - row[(2*i + 1)])/2;
 		}
-		
+
 		return newRow;
 	}
-	
+
 	public double[][] getMatrix(){
 		return matrix;
 	}
--- src/ImageMatrixIO.java
+++ src/ImageMatrixIO.java
@@ -4,13 +4,14 @@ import java.awt.image.BufferedImage;
 
 public class ImageMatrixIO {
 	public static BufferedImage matToImage(double[][] mat){
-		int w = mat[0].length;
-		int h = mat.length;
-		
+		int h = mat[0].length;
+
+		int w = mat.length;
+
 		BufferedImage img = new BufferedImage(w,h,BufferedImage.TYPE_INT_RGB);
-		
-		for( int i = 0 ; i < w ; i++){
-			for (int j = 0 ; j < h ; j++){
+
+		for( int i = 0 ; i < w; i++){
+			for (int j = 0 ; j < h; j++){
 				int value = (int) Math.abs(mat[i][j]);
 				int color;
 				if (value > 5){
@@ -24,19 +25,29 @@ public class ImageMatrixIO {
 		}
 		return img;
 	}
-	
+
 	public static double[][] imageToMat(BufferedImage img){
 		// Initializing the matrix
-		double[][] mat = new double[img.getHeight()][img.getWidth()];
-		
+		double[][] mat = new double[img.getWidth()][img.getHeight()];
+		System.out.println(img.getWidth() + "width");
+		System.out.print(img.getHeight());
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+
 		// Filling the matrix
-		for (int i = 0 ; i < img.getWidth() ; i++){
-			for (int j = 0 ; j < img.getHeight() ; j++){
-				Color grayColor = new Color(img.getRGB(i,j));	
+		for (int i = 0 ; i < img.getWidth(); i++){
+			for (int j = 0 ; j < img.getHeight(); j++){
+				Color grayColor = new Color(img.getRGB(i,j));
 				mat[i][j] = (double) grayColor.getRed();
 			}
 		}
-		
+
 		return mat;
 	}
 }
